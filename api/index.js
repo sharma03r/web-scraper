9f03e38e195f07c0a0db5de5b06210e8b4388360
Rahul Sharma <sharmarahul8848@gmail.com>
2024-05-11 09:27:45 +0530

    Added data capture functionality

--- api/index.js
+++ api/index.js
@@ -1,1 +1,13 @@
 import express from "express";
+const app = express();
+const port = process.env.port || 8080;
+import scrapeProducts from "./scraper.js";
+app.use(express.json());
+app.use("/", scrapeProducts);
+try {
+  const httpServer = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+} catch (error) {
+  console.log(error);
+}
--- api/scraper.js
+++ api/scraper.js
@@ -3,14 +3,32 @@ import cheerio from "cheerio";
 import axios from "axios";
 
 const router = express.Router();
-const baseUrl = "https://www.amazon.com";
+const baseUrl = "https://crawler-test.com";
 
 router.post("/scrape", async (req, res) => {
+  console.log("hi");
+  console.log(req.body.url);
   const { url } = req.body;
+
   if (!url.includes(baseUrl)) {
     console.log("Invalid URL");
     return res.status(404).json({
       message: "Invald URL",
     });
   }
+  try {
+    let data;
+    axios.get(url).then((response) => {
+      // Load the HTML into cheerio
+      data = cheerio.load(response.data);
+      console.log(response);
+    });
+    return res.status(200).json({
+      text: "hi",
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
+
+export default router;
