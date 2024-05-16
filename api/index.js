c135babb02e28a81525048f040de85860b28d26b
Rahul Sharma <sharmarahul8848@gmail.com>
2024-05-16 11:12:35 +0530

    Added functionality to spin worker nodes

--- api/index.js
+++ api/index.js
@@ -1,13 +1,25 @@
 import express from "express";
 const app = express();
-const port = process.env.port || 8080;
+import cluster from "node:cluster";
+
 import scrapeProducts from "./scraper.js";
+
+const port = process.env.port || 8080;
 app.use(express.json());
 app.use("/", scrapeProducts);
-try {
-  const httpServer = app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-} catch (error) {
-  console.log(error);
+if (cluster.isPrimary) {
+  // Fork workers
+  for (let i = 0; i < 4; i++) {
+    cluster.fork();
+  }
+} else {
+  // Worker process
+  // Create an HTTP server
+  try {
+    const httpServer = app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
--- api/scraper.js
+++ api/scraper.js
@@ -1,11 +1,12 @@
 import express from "express";
-import cheerio from "cheerio";
+import { load } from "cheerio";
 import axios from "axios";
 
 const router = express.Router();
 const baseUrl = "https://crawler-test.com";
 
 router.post("/scrape", async (req, res) => {
+  console.log(`Process running on worker process with id ${process.pid}`);
   console.log("hi");
   console.log(req.body.url);
   const { url } = req.body;
@@ -20,11 +21,11 @@ router.post("/scrape", async (req, res) => {
     let data;
     axios.get(url).then((response) => {
       // Load the HTML into cheerio
-      data = cheerio.load(response.data);
-      console.log(response);
-    });
-    return res.status(200).json({
-      text: "hi",
+      data = load(response.data);
+      return res.status(200).json({
+        text: "hi",
+        process_Id: process.pid,
+      });
     });
   } catch (error) {
     console.log(error);
